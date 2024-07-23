36eced090170af92aa4dec92f4e7653cea1fede9
Zinhle Buhlungu <zinhle.buhlungu@mondiadigital.com>
2024-07-23 16:04:22 +0200

    Add Sidebar

---
+++ src/app/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+"use client";
+
+export default function Sidebar() {
+
+
+  return (
+    <aside className="sticky top-0 h-fit rounded-lg border bg-background p-4 md:w-[260px]">
+    
+    </aside>
+  );
+}
